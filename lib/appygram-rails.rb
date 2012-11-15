6f8b6842ac95b5a84cc1af01e4ed9501d26e789e
Rob Heittman <heittman.rob@gmail.com>
2012-11-15 15:26:56 -0500

    More configurable ignore-list
    AppygramRails.ignore_exception_class_name! 'IgnoreThisException'

--- lib/appygram-rails.rb
+++ lib/appygram-rails.rb
@@ -12,4 +12,26 @@ module AppygramRails
 
   end
 
+  @@ignored_classes = [
+    'ActiveRecord::RecordNotFound',
+    'ActiveController::RoutingException'
+  ]
+
+  class << self
+
+    def ignore_exception_class_name!(k)
+      @@ignored_classes << k
+    end
+
+    def ignores_exception_class?(exception)
+      @@ignored_classes.each do |k|
+        if exception.class.name == k
+          return true
+        end
+      end
+      false
+    end
+
+  end
+
 end
--- lib/appygram-rails/integration/rack_rails.rb
+++ lib/appygram-rails/integration/rack_rails.rb
@@ -8,20 +8,22 @@ module Rack
       @app = app
     end
 
+    def handle_with_controller(exception, controller, request)
+      unless ::AppygramRails.ignores_exception_class? exception
+        ::AppygramRails::Catcher.handle_with_controller(e,env['action_controller.instance'], Rack::Request.new(env))
+      end
+    end
+
     def call(env)
       begin
         body = @app.call(env)
       rescue Exception => e
-        unless e.is_a? ActionController::RoutingError
-          ::AppygramRails::Catcher.handle_with_controller(e,env['action_controller.instance'], Rack::Request.new(env))
-        end
+        handle_with_controller(e,env['action_controller.instance'], Rack::Request.new(env))
         raise
       end
 
       if env['rack.exception']
-        unless env['rack.exception'].is_a? ActionController::RoutingError
-          ::AppygramRails::Catcher.handle_with_controller(env['rack.exception'],env['action_controller.instance'], Rack::Request.new(env))
-        end
+        handle_with_controller(env['rack.exception'],env['action_controller.instance'], Rack::Request.new(env))
       end
 
       body
